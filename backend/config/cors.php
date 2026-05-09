18a2cac7d1701085f785da817d2dd7e754e9ce07
ShahedAltrakiah <shahedaltrakiah@gmail.com>
2026-05-10 01:20:21 +0300

    push

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -2,7 +2,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002'],
+    'allowed_origins' => ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:3002', 'https://marsafounders.com'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
